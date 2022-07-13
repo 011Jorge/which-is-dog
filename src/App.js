13b0f6c2571e79c05dcc59cc7ad4f5f4d6e350e5
011Jorge <vicentejorge187@gmail.com>
2022-07-13 20:05:20 -0300

    styling simple

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 import axios from "axios";
 
-import { Container, H1, ContainerItems, Dogs } from "./styles";
+import { Container, H1, ContainerItems, Button, About } from "./styles";
 
 function App() {
   const [dogs, setDogs] = useState([]);
@@ -10,7 +10,7 @@ function App() {
   async function listDogs() {
     const { data } = await axios.get("https://api.thedogapi.com/v1/breeds");
 
-    console.log(data[50]);
+    console.log(data);
 
     setDogs([
       {
@@ -24,19 +24,19 @@ function App() {
 
   return (
     <Container>
-      <H1>The Dog Api</H1>
-      <button onClick={listDogs}>teste</button>
+      <H1>Qual é o Dog ?</H1>
+      <Button onClick={listDogs}>Teste</Button>
 
       <ContainerItems>
         <ul>
           {dogs.map((dog) => {
             return (
-              <div key={dog.id}>
+              <About key={dog.id}>
                 <h1>{dog.name}</h1>
                 <h2>{dog.about}</h2>
                 <p>{dog.temperament}</p>
                 <img src={dog.image} alt="test" />
-              </div>
+              </About>
             );
           })}
         </ul>
--- src/styles.js
+++ src/styles.js
@@ -1,9 +1,49 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 50px;
+`;
 
 export const H1 = styled.h1``;
 
 export const ContainerItems = styled.div``;
 
-export const Dogs = styled.div``;
+export const Button = styled.button`
+  width: 150px;
+  height: 50px;
+  margin-top: 30px;
+  border: none;
+  outline: none;
+  border-radius: 10px;
+  cursor: pointer;
+  font-size: 18px;
+  font-weight: 500;
+  background-color: #964b00;
+  color: #fff;
+
+  &:hover {
+    opacity: 0.8;
+  }
+
+  &:active {
+    opacity: 0.6;
+  }
+`;
+
+export const About = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 50px 0;
+
+    p {
+        padding-top: 10px;
+    }
+
+    img {
+        padding-top: 20px;
+    }
+`;
