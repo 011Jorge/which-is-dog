64b0fec4059305498fff32b5c835543bf330bc98
011Jorge <vicentejorge187@gmail.com>
2022-07-13 19:29:58 -0300

    capturing and using API data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+
+import axios from "axios";
+
+import { Container, H1, ContainerItems, Dogs } from "./styles";
 
 function App() {
-  const hello = "Hello World!"
+  const [dogs, setDogs] = useState([]);
 
-  return <h1>{hello}</h1>;
+  async function listDogs() {
+    const { data } = await axios.get("https://api.thedogapi.com/v1/breeds");
+
+    console.log(data[50]);
+
+    setDogs([
+      {
+        name: data[50].name,
+        about: data[50].bred_for,
+        temperament: data[50].temperament,
+        image: data[50].image.url,
+      },
+    ]);
+  }
+
+  return (
+    <Container>
+      <H1>The Dog Api</H1>
+      <button onClick={listDogs}>teste</button>
+
+      <ContainerItems>
+        <ul>
+          {dogs.map((dog) => {
+            return (
+              <div key={dog.id}>
+                <h1>{dog.name}</h1>
+                <h2>{dog.about}</h2>
+                <p>{dog.temperament}</p>
+                <img src={dog.image} alt="test" />
+              </div>
+            );
+          })}
+        </ul>
+      </ContainerItems>
+    </Container>
+  );
 }
 
 export default App;
---
+++ src/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+export const H1 = styled.h1``;
+
+export const ContainerItems = styled.div``;
+
+export const Dogs = styled.div``;
