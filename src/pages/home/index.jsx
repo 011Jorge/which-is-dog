13f4b19ee0f85ccfa5e517a903306af315e089e3
011Jorge <vicentejorge187@gmail.com>
2022-11-06 18:05:21 -0300

    :construction: refreshing the first session of the home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,29 +1,31 @@
 import React from "react";
+import { FaLongArrowAltRight } from "react-icons/fa";
+
 import Navbar from "../../components/navbar";
-import cookiesSvg from "../../svg/cookies.svg";
 import arrowSvg from "../../svg/arrow.svg";
-import categorySvg from "../../svg/category.svg";
+import dogSvg from "../../svg/dog.svg";
 
 function Home() {
   return (
-    <div className="container-home ">
+    <div className="container-home">
       <Navbar />
-      <div className="about-container space-between flex ">
-        <div className="info-home ml-9">
+      <div className="container-about-home space-between flex">
+        <div className="container-info-home m-9">
           <h1>
-            CodeDog <img src={arrowSvg} className="img-arrow" alt="arrow-img" />
+            CodeDog
+            <img src={arrowSvg} className="img-arrow ml-2" alt="arrow-img" />
           </h1>
           <h2 className="mt-2">Descubra Diferentes</h2>
           <h3>
             Informações e curiosidades <br /> sobre diversas raças de cachorros.
           </h3>
+          <a href="#">
+            <button className="pl-7 mt-4 flex">
+              Descobrir dogs <FaLongArrowAltRight className="ml-2" />
+            </button>
+          </a>
         </div>
-        <img
-          src={categorySvg}
-          className="img-category"
-          alt="categories-image"
-        />
-        <img src={cookiesSvg} className="img-cookies" alt="cookies-image " />
+        <img src={dogSvg} className="img-dog" alt="img-dog" />
       </div>
     </div>
   );
