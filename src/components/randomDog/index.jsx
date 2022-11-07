13b091be72e6da8a036ff92d928608fc8e5471cb
011Jorge <vicentejorge187@gmail.com>
2022-11-07 19:58:10 -0300

    :construction: building randomDog component

--- src/components/randomDog/index.jsx
+++ src/components/randomDog/index.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 
+import { FaPlus } from "react-icons/fa";
+
 function RandomDog() {
   return (
     <div className="container-randomDog">
-      <h1>I am RandomDog</h1>
+      <h1 className="pt-5">RandomDog</h1>
+
+      <div className="container-dogData p-7">
+        <div className="container-imageDog"></div>
+        <div className="ml-9 p-4">
+          <button className="button-randomDog my-3 pl-9 flex">
+            New Dog <FaPlus className="iconPlus m-1" />
+          </button>
+          <div className="container-infoDog"></div>
+        </div>
+      </div>
     </div>
   );
 }
